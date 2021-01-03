e62d0ab115f09be19a14caab6c66bf1f92618ee7
iswansoftware <46799776+iswansoftware@users.noreply.github.com>
2021-01-03 17:09:15 +0700

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -1,6 +1,6 @@
 window.onload = function () {
     const useNodeJS = false;   // if you are not using a node server, set this value to false
-    const defaultLiffId = "1655326479-LRn341QY";   // change the default LIFF value if you are not using a node server
+    const defaultLiffId = "1655540382-2X5XM7K8";   // change the default LIFF value if you are not using a node server
 
     // DO NOT CHANGE THIS
     let myLiffId = "";
